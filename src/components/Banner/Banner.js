11e83706dd7db0cd9b4e92228579451756858a06
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-04-13 21:18:44 -0600

    Update banner component with the StartButton new props

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,19 +1,24 @@
+import React, { useState } from 'react';
+import useVisibilityState from '../../hooks/useVisibilityState';
+import StartButton from '../StartButton/StartButton';
 import './Banner.css';
 import worldCupTrohpy from '../../assets/images/world-cup-trophy.png';
 
 function Banner() {
+    const [visible, setVisible] = useVisibilityState(true);
+
     return(
         <div className="banner-container banner-background alert alert-dark container">
             <div className="row">
                 <div className="banner-text col-4 text-end">
-                    <img src={worldCupTrohpy} className="world-cup"/>
+                    <img src={worldCupTrohpy} className="world-cup" alt="world-cup"/>
                 </div>
                 <div className="banner-text col-4 text-center">
                     <div>Memo Test</div>
-                    <button className="btn btn-success">Start Game</button>
+                    <StartButton value="Start Game" visible={visible} onClick={setVisible}/>       
                 </div>
                 <div className="banner-text col-4 text-start">
-                    <img src={worldCupTrohpy} className="world-cup"/>
+                    <img src={worldCupTrohpy} className="world-cup" alt="world-cup"/>
                 </div>
             </div>
         </div>

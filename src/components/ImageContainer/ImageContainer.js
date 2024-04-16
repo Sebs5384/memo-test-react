9c119b6251f767218e07fd64e87357341bfbaed9
Sebs5384 <operationmadjackal@hotmail.com>
2024-04-16 11:33:52 -0300

    Add propTypes to image component

--- src/components/ImageContainer/ImageContainer.js
+++ src/components/ImageContainer/ImageContainer.js
@@ -1,9 +1,15 @@
 import './ImageContainer.css';
+import PropTypes from 'prop-types';
 
 function ImageContainer({ src, alt, className }) {
     return(
         <img src={src} alt={alt} className={className}/>
     );  
 };
+ImageContainer.propTypes = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    className: PropTypes.string
+}
 
 export default ImageContainer;

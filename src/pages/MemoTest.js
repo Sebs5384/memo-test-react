bce25705710aaaaa55474304f791a6c7a19ca514
Sebs5384 <operationmadjackal@hotmail.com>
2024-04-16 11:36:17 -0300

    Set cards in memotest component
    Using the functions we made before, we giving a new array which contains the cards to our component Board

--- src/pages/MemoTest.js
+++ src/pages/MemoTest.js
@@ -1,8 +1,24 @@
+import React, { useState } from 'react';
 import Banner from "../components/Banner/Banner"
+import Board from "../components/Board/Board"
+import { getPlayerCards, getSetOfCards, shuffleArray } from "../utils/general"
 
 function Memotest() {
+    const playerCards = getPlayerCards();
+    const shuffledPlayerCards = shuffleArray(playerCards);
+    const setOfCards = getSetOfCards(shuffledPlayerCards);
+    const shuffledSetOfCards = shuffleArray(setOfCards);
+
+    console.log(shuffledSetOfCards);
+
+    const [cards, setCards] = useState(shuffledSetOfCards);
+    const [flipped, setFlipped] = useState(false);
+
     return(
-        <Banner value={"Memotest"}></Banner>
+        <>
+            <Banner value={"Memotest"}></Banner>
+            <Board cards={cards} flipped={flipped} setFlipped={setFlipped}></Board>
+        </>
     );
 };
 

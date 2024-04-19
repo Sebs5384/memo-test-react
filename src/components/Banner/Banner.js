256a87ed1a66ae51ecc3338437d14529899fc5a3
Sebs5384 <operationmadjackal@hotmail.com>
2024-04-18 21:02:49 -0300

    Update banner by changing its props

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
 import StartButton from '../StartButton/StartButton';
 import ImageContainer from '../ImageContainer/ImageContainer';
-import useVisibilityState from '../../hooks/useVisibilityState';
 import worldCupTrohpy from '../../assets/images/misc/world-cup-trophy.png';
 import PropTypes from 'prop-types';
 import './Banner.css';
 
 
-function Banner({value}) {
-    const [visible, setVisible] = useVisibilityState(true);
-
+function Banner({visibility, startGame}) {
     return(
         <div className="banner-container banner-background alert alert-dark container">
             <div className="row">
@@ -17,8 +13,8 @@ function Banner({value}) {
                     <ImageContainer src={worldCupTrohpy} alt={"world-cup"} className={"world-cup"}/>
                 </div>
                 <div className="banner-text col-4 text-center">
-                    <div>{value}</div>
-                    <StartButton value="Start Game" visible={visible} onClick={setVisible}/>       
+                    <div>Memotest</div>
+                    <StartButton value="Start Game" visible={visibility} onClick={startGame}/>       
                 </div>
                 <div className="banner-text col-4 text-start">
                     <ImageContainer src={worldCupTrohpy} alt={"world-cup"} className={"world-cup"}/>
@@ -28,7 +24,8 @@ function Banner({value}) {
     );
 };
 Banner.propTypes = {
-    value: PropTypes.string
-}
+    visibility: PropTypes.bool,
+    setVisibility: PropTypes.func
+};
 
 export default Banner;

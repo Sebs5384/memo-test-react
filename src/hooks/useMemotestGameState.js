5b7def28b6ca3657ba78dd197e3790c6df1e0afb
Sebs5384 <operationmadjackal@hotmail.com>
2024-04-19 23:55:46 -0300

    Update memotest hook to only work around the state not visibility

--- src/hooks/useMemotestGameState.js
+++ src/hooks/useMemotestGameState.js
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
-import useVisibilityState from "./useVisibilityState";
 import { getPlayerCards } from "../utils/general"
 
 function useMemotestGameState({ initialValue }) {
     const [gameStarted, setGameStarted] = useState(initialValue);
-    const [visibility, setVisibility] = useVisibilityState(true);
     const cards = gameStarted ? getPlayerCards() : [];
 
     const startGame = () => {    
-        setVisibility(false);
         setGameStarted(!gameStarted);
     };
     
 
-    return { gameStarted, cards, visibility, startGame };
+    return { gameStarted, cards, startGame };
 };
 
 export default useMemotestGameState;

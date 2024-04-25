c5d2958a5c1e375fa0bb53c32142e04d8cd1818f
Sebs5384 <operationmadjackal@hotmail.com>
2024-04-25 20:27:06 -0300

    Remove className prop from Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,20 +4,29 @@ import "./Card.css";
 import cx from "classnames";
 import PropTypes from "prop-types";
 
-function Card({ image, unflippedCard, className }) {
+function Card({ image, unflippedCard }) {
     const { flipped, handleFlippedCard } = useCardState(false);
 
     return(
         <>
-            <ImageContainer src={unflippedCard} alt={"unflipped-card"} className={cx("memo-card", className, {"flipped" : flipped})} onClick={handleFlippedCard}/>
-            <ImageContainer src={image} alt={"card"} className={cx("memo-card", className, {"flipped": flipped})} onClick={handleFlippedCard}/>
+            <ImageContainer 
+                src={unflippedCard} 
+                alt={"unflipped-card"} 
+                className={cx("memo-card front-card", {"flipped" : flipped})} 
+                onClick={handleFlippedCard}
+            />
+            <ImageContainer 
+                src={image} 
+                alt={"card"} 
+                className={cx("memo-card overlay-card", {"flipped": flipped})} 
+                onClick={handleFlippedCard}
+            />
         </>
     );
 };
 Card.propTypes = {
     image: PropTypes.string,
-    unflippedCard: PropTypes.string,
-    className: PropTypes.string
+    unflippedCard: PropTypes.string
 };
 
 export default Card;

f2bc9f3928d14e5a107cc68207cf6a76d1ee2d09
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-05-13 18:02:29 -0600

    Removes useFlipState
    We are adding the flip className directly if the card is between the array of active cards or matchedPairs

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,5 @@
 import useMemotestGameState from "./useMemotestGameState";
 import useMatchingState from "./useMatchingState";
 import useVisibilityState from "./useVisibilityState";
-import useFlipState from "./useFlipState";
 
-export { useMemotestGameState, useMatchingState, useVisibilityState, useFlipState };
+export { useMemotestGameState, useMatchingState, useVisibilityState };

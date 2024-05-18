ae449ed429ff4bbfa74a276f003430b983244b18
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-05-17 23:51:26 -0600

    Update matching hook with gameEnded
    This will set a new state "gameEnded" when the game has reached the max pairs of cards matched in the game

--- src/hooks/useMatchingState.js
+++ src/hooks/useMatchingState.js
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function useMatchingState() {
     const [activeCards, setActiveCards] = useState([]);
     const [matchedPairs, setMatchedPairs] = useState([]);
+    const [gameEnded, setGameEnded] = useState(false);
+    const MAX_MATCHED_PAIRS = 12;
     const MAX_FLIPPED_CARDS = 2;
 
     const handleMatching = (key, index) => {
-        
+            
         const belowMaxFlippedCards = activeCards.length < MAX_FLIPPED_CARDS
         if(belowMaxFlippedCards) {
             setActiveCards([...activeCards, key]);
-                
+                    
             const activeCardsLength = activeCards.length + 1;
 
             if(activeCardsLength === MAX_FLIPPED_CARDS) {
@@ -18,8 +20,7 @@ function useMatchingState() {
                 const previousCard = activeCards[0].replace(/-[^-]*$/, "")
                 const currentCardSuffix = key.split("-")[3];
                 const currentCard = key.replace(/-[^-]*$/, "")
-                
-
+                    
                 if(previousCard === currentCard) {
                     setMatchedPairs([...matchedPairs, `${previousCard}-${previousCardSuffix}`, `${currentCard}-${currentCardSuffix}`]);
                     setActiveCards([]);
@@ -30,7 +31,13 @@ function useMatchingState() {
         };
     };
 
-    return { activeCards, matchedPairs, handleMatching }
+    useEffect(() => {
+        if(matchedPairs.length === MAX_MATCHED_PAIRS) {
+            setGameEnded(true);
+        };
+    }, [matchedPairs]);
+
+    return { activeCards, matchedPairs, gameEnded, handleMatching }
 };
 
 export default useMatchingState;

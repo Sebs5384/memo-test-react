c5942c1b461744f0f7579e739c87d352ac739e9b
Sebs5384 <operationmadjackal@hotmail.com>
2024-05-21 18:37:56 -0300

    Setup opacity state when gameEnded
    This will turn the card opacity to 0 and then go back to 1 bringing them back, the idea is to use this on the board and banner component once the user wins, bringing back the whole cards rather than we one we used during the game

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,13 +1,42 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getPlayerName } from "../../utils/general";
 import Card from "../Card/Card";
 import "./Board.css";
 import PropTypes from "prop-types";
 
 function Board({ gameStarted, gameEnded, cards, backCardSprite, activeCards, matchedPairs, handleMatching }) {
+    const [opacity, setOpacity] = useState(1);
+    const [opacityTransition, setOpacityTransition] = useState("decrease");
+
+    useEffect(() => {
+        if(gameEnded) {
+            const opacityChange = setInterval(() => {
+                setOpacity(prevOpacity => {
+                    if(opacityTransition === "decrease") {
+                        const newOpacity = prevOpacity - 0.1;
+                        if(newOpacity <= 0) {
+                            setOpacityTransition('increase');
+                            return 0;
+                        };
+                        return newOpacity;
+                    } else  {
+                        const newOpacity = prevOpacity + 0.1;
+                        if(newOpacity >= 1) {
+                            clearInterval(opacityChange);
+                            return 1;
+                        };
+                        return newOpacity;
+                    };
+                });
+            }, 1500);
+
+            return () => clearInterval(opacityChange);
+        };
+    }, [gameEnded, opacityTransition])
+
     return (
         <div className={"container text-center"}>
-            <div id="board">
+            <div id="board" style={{opacity: opacity}}>
                 <div className="row gx-1">
                     {cards.map((card, index) => {
                         const { playerSpriteName, playerNameWithSuffix } = getPlayerName(card);

be40e79c93d98f5027a30d95c05af2712ff18f9b
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-05-25 21:32:07 -0600

    Updates the game with a hook that changes the background upon winning

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,10 +2,12 @@ import useMemotestGameState from "./useMemotestGameState";
 import useMatchingState from "./useMatchingState";
 import useVisibilityState from "./useVisibilityState";
 import useOpacityState from "./useOpacityState";
+import useBodyBackground from "./useBodyBackground";
 
 export { 
     useMemotestGameState, 
     useMatchingState, 
     useVisibilityState, 
-    useOpacityState 
+    useOpacityState,
+    useBodyBackground 
 };
--- src/pages/MemoTest.js
+++ src/pages/MemoTest.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner/Banner"
 import Board from "../components/Board/Board"
-import { useMemotestGameState, useMatchingState, useOpacityState } from "../hooks/index";
+import { useMemotestGameState, useMatchingState, useOpacityState, useBodyBackground } from "../hooks/index";
 
 function Memotest() {
     const { gameStarted, gameRestarted, cards, unflippedCardSprite, startGame, restartGame } = useMemotestGameState({ initialValue: false });
     const { activeCards, matchedPairs, gameEnded, handleMatching } = useMatchingState();
     const { opacity } = useOpacityState({ gameEnded });
+
+    useBodyBackground({ gameHasEnded: gameEnded });
 
     return(
         <>

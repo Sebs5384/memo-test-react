e73c87f2d2540533e35f035ad9d64fbad6c964ff
Sebs5384 <operationmadjackal@hotmail.com>
2024-06-03 18:56:05 -0300

    Removes useEffect from visibility state
    We are not using this

--- src/hooks/useVisibilityState.js
+++ src/hooks/useVisibilityState.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function useVisibilityState(initialValue) {
     const [visible, setVisible] = useState(initialValue);
